745c1ae07c6316d044b0e7750acb8fb549898eeb
Srujan <130295979+srujan-0@users.noreply.github.com>
2026-03-06 14:48:07 +0530

    Add Demo1 class to instantiate classes from p2

---
+++ WEEK-4/Demo1.java
@@ -0,0 +1,11 @@
+package p1;
+// Instantiate the various classes in p2.
+public class Demo1
+{
+public static void main(String args[])
+{
+Protection ob1 = new Protection();
+Derived ob2 = new Derived();
+SamePackage ob3 = new SamePackage();
+}
+}
